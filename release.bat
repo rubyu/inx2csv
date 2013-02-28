277ead1857184abdb4371218077539efb9d94bfe
rubyu <ruby.u.g@gmail.com>
2013-03-01 01:12:51 +0900

    fix release.bat

--- release.bat
+++ release.bat
@@ -1,5 +1,7 @@
-del inx2csv
 del inx2csv.zip
+rmdir /s /q dist
+rmdir /s /q inx2csv
 python setup.py py2exe
+ping localhost -n 5 > nul
 rename dist inx2csv
 "C:\Program Files\7-Zip\7z.exe" a inx2csv.zip inx2csv
